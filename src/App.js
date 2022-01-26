782c5e588f2a3dc9995a376fdd78dd45a338cc8c
yootaek <eldbxor@naver.com>
2022-01-26 16:50:01 +0900

    함수형 immer 사용

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     (e) => {
       const { name, value } = e.target;
       setForm(
-        produce(form, (draft) => {
+        produce((draft) => {
           draft[name] = value;
         })
       );
@@ -34,7 +34,7 @@ const App = () => {
 
       // array에 새 항목 등록
       setData(
-        produce(data, (draft) => {
+        produce((draft) => {
           draft.array.push(info);
         })
       );
@@ -53,7 +53,7 @@ const App = () => {
   const onRemove = useCallback(
     (id) => {
       setData(
-        produce(data, (draft) => {
+        produce((draft) => {
           draft.array.splice(
             draft.array.findIndex((info) => info.id === id),
             1
